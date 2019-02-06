1586e29c45b8a3afa6a82dce306e583a9a4567ec
matthewmahler <matthewjamesmahler@gmail.com>
2019-02-06 06:19:22 -0500

    testing

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const about = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>About Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi dicta
+          officiis ab explicabo impedit recusandae voluptate repellendus ipsam
+          laudantium doloremque beatae ipsa perferendis cum labore laboriosam
+          iure, perspiciatis corporis ut?
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default about;
